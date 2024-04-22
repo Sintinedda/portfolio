0b31c44f04063e408eda562893f79da5276e0cc1
Jeremy Aboudan <aboudan.jeremy@gmail.com>
2024-04-22 14:50:46 +0200

    404

---
+++ src/components/404.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>404 Not found.</h1>
+    <div><img src="../assets/img/sorry.gif" alt=""></div>
+    <p>La page que vous recherchez n'existe pas.</p>
+</template>
+
+<style scoped>
+    h1{
+        font-size: 3rem;
+        text-align: center;
+        padding-top: 200px;
+        padding-bottom: 50px;
+    }
+
+    div{
+        text-align: center;
+    }
+
+    img{
+        width: 400px;
+        border-radius: 2%;
+    }
+
+    p{
+        color: gold;
+        text-align: center;
+        font-size: 2rem;
+        padding-top: 50px;
+    }
+</style>
