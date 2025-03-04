3c7624ac91dc15730dfcb471ddef355a2731b173
Jeremy Aboudan <aboudan.jeremy@gmail.com>
2025-03-04 15:12:04 +0100

    update

--- src/components/TheModal.vue
+++ src/components/TheModal.vue
@@ -4,32 +4,74 @@
     import '../assets/modal.css'
     const works = ref([
         {
-        nom: "Curriculum Vitae",
+            nom: "Curriculum Vitae",
             date: "16/01/2024",
             gitHub: "https://github.com/Sintinedda/Curriculum-vitae-Jeremy-Aboudan-",
             pdf : "../../public/monCv.pdf",
             image: "work-1"
         },
         {
-        nom: "Cahier des charges",
+            nom: "Cahier des charges",
             date: "06/02/2024",
             pdf : "../../public/cahierDesCharges.pdf",
             image: "work-2"
         },
         {
-        nom: "Espace commentaire dynamique",
+            nom: "Espace commentaire dynamique",
             date: "18/02/2024",
             gitHub: "https://https://github.com/Sintinedda/Dynamiser-un-espace-commentaire",
             pdf : "../../public/espaceCommentaireDynamique.pdf",
             image: "work-3"
         },
         {
-        nom: "Mon site Web",
-            date: "Toujours en développement",
-            gitHub: "https://github.com/Sintinedda/mon-site-web",
-            pdf : "../../public/monSiteWeb.pdf",
-            image: "work-4"
+            nom: "tifosi",
+            date: "10/10/2024",
+            gitHub: "https://github.com/Sintinedda/tifosi",
+            image: "sql"
         },
+        {
+            nom: "Au petit village",
+            date:  "22/07/2024",
+            gitHub: "https://github.com/Sintinedda/au-petit-village",
+            pdf: "../../public/AuPetitVillage.pdf",
+            image: "angular"
+        },
+        {
+            nom: "Trouve ton artisan",
+            date: "30/08/2024",
+            gitHub: "https://github.com/Sintinedda/trouve-ton-artisan",
+            image: "react"
+        },
+        {
+            nom: "Librairie",
+            date: "01/11/2024",
+            gitHub: "https://github.com/Sintinedda/Library",
+            image: "python"
+        },
+        {
+            nom: "Russell API",
+            date: "03/12/2024",
+            gitHub: "https://github.com/Sintinedda/Russell-API",
+            image: "nodejs"
+        },
+        {
+            nom: "Stubborn",
+            date: "17/02/2025",
+            gitHub: "https://github.com/Sintinedda/StubbornCefProject",
+            image: "symfony"
+        },
+        {
+            nom: "Mon portfolio",
+            date: "17/02/2025",
+            gitHub: "https://github.com/Sintinedda/portfolio",
+            image: "vuejs"
+        },
+        {
+            nom: "Projet perso",
+            date: "en cours",
+            gitHub: "https://github.com/Sintinedda/my_site_web_symfony",
+            image: "work-4"
+        }
     ])
     const selectedProject = ref({});
     const isModalOpened = ref(false);
